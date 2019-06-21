74a99dc91866c412c2214e023a5df74c231bf100
Brandon Harris <bharris@dminc.com>
2019-06-21 18:08:34 -0500

    Add wwc team boxscore objects

--- scores-api/src/maps/womensWorldCupMatch.js
+++ scores-api/src/maps/womensWorldCupMatch.js
@@ -1,8 +1,9 @@
-import { getTvBroadcast } from './scoreHelpers';
+import { getTeamScore, getTvBroadcast } from './scoreHelpers';
 
 const mapMatchToInternalModel = event => {
   return {
     id: event.header.id,
+    shortName: getShortName(event.header),
     date: event.header.competitions[0].date,
     completed: event.header.competitions[0].status.type.completed,
     period: event.header.competitions[0].status.period,
@@ -10,29 +11,54 @@ const mapMatchToInternalModel = event => {
     status: event.header.competitions[0].status.type.shortDetail,
     note: event.header.season.name,
     tvBroadcast: getTvBroadcast(event.header.competitions[0].broadcasts),
-    header: event.header.competitions[0].competitors.map(scoreItem => ({
-      homeAway: scoreItem.homeAway,
-      score: scoreItem.score,
-      hits: scoreItem.hits,
-      errors: scoreItem.errors,
-      winner: scoreItem.winner,
-      teamAbbreviation: scoreItem.team.abbreviation,
-      team: scoreItem.team.displayName,
-      logo: scoreItem.team.logos[0].href,
-      lineScores: scoreItem.linescores
-    })),
-    teamStats: event.boxscore.teams.map(teamBox => ({
-      team: teamBox.team.displayName,
-      logo: teamBox.team.logo,
-      stats: teamBox.statistics
-    })),
+    homeScore: mapScore(getTeamScore(event.header, 'home')),
+    awayScore: mapScore(getTeamScore(event.header, 'away')),
+    homeStats: mapTeamStats(getTeamStats(event.boxscore.teams, event.header, 'home')),
+    awayStats: mapTeamStats(getTeamStats(event.boxscore.teams, event.header, 'away')),
     goals: getGoals(event.header.competitions[0].details)
   };
 };
 
+function getShortName(header) {
+  const homeTeam = getTeam(header, 'home');
+  const awayTeam = getTeam(header, 'away');
+  return `${awayTeam.team.abbreviation} @ ${homeTeam.team.abbreviation}`;
+}
+
+function mapTeamStats(stats) {
+  return {
+    team: stats.team.displayName,
+    logo: stats.team.logo,
+    stats: stats.statistics
+  }
+}
+
+function getTeam(header, homeAway) {
+  return header.competitions[0].competitors.find(
+    team => team.homeAway === homeAway
+  );
+}
+
+function getTeamStats(boxScores, header, homeAway) {
+  const team = getTeam(header, homeAway);
+  return boxScores.find(p => p.team.id === team.id);
+}
+
+function mapScore(scoreItem) {
+  return {
+    homeAway: scoreItem.homeAway,
+    score: scoreItem.score,
+    winner: scoreItem.winner,
+    teamAbbreviation: scoreItem.team.abbreviation,
+    team: scoreItem.team.displayName,
+    logo: scoreItem.team.logos[0].href,
+    lineScores: scoreItem.linescores
+  };
+}
+
 function getGoals(details) {
   if (!!details) {
-    return event.header.competitions[0].details.map(goal => ({
+    return details.map(goal => ({
       clock: goal.clock.displayValue,
       team: goal.team.displayName,
       ownGoal: goal.ownGoal,
