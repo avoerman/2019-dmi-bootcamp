024bfc34d149483f500119c704a5cf7aeb9bed4b
bharris9 <brandonkharris@gmail.com>
2019-06-22 17:16:53 -0500

    Merge API updates (#13)
    * Update MLB models
    
    * Fix MLB model

--- scores-api/src/maps/mlb.js
+++ scores-api/src/maps/mlb.js
@@ -15,9 +15,42 @@ const mapToInternalModel = data => {
     tvBroadcast: getTvBroadcast(event.competitions[0].geoBroadcasts),
     homeScore: mapScore(getTeamScore(event, 'home')),
     awayScore: mapScore(getTeamScore(event, 'away')),
-    odds: getOdds(event.competitions[0].odds)
+    odds: getOdds(event.competitions[0].odds),
+    lastPlay: getLastPlay(event.competitions[0].situation),
+    atBat: getAtBat(event.competitions[0].situation),
+    currentSituation: getCurrentSituation(event.competitions[0].situation)
   }));
 };
+
+function getAtBat(situation) {
+  if(!!situation && !!situation.batter && !!situation.pitcher) {
+    const pitcher = situation.pitcher.athlete.shortName;
+    const batter = `${situation.batter.athlete.shortName}`;
+    return `${pitcher} pitching to ${batter}`
+  }
+  return null;
+}
+
+function getLastPlay(situation) {
+  if (!!situation) {
+    return situation.lastPlay.text;
+  }
+  return null;
+}
+
+function getCurrentSituation(situation) {
+  if (!!situation) {
+    return {
+      balls: situation.balls,
+      strikes: situation.strikes,
+      outs: situation.outs,
+      onFirst: !!situation.onFirst,
+      onSecond: !!situation.onSecond,
+      onThird: !!situation.onThird
+    }
+  }
+  return null;
+}
 
 function mapScore(scoreItem) {
   return {
@@ -28,7 +61,8 @@ function mapScore(scoreItem) {
     winner: scoreItem.winner,
     teamAbbreviation: scoreItem.team.abbreviation,
     team: scoreItem.team.displayName,
-    logo: scoreItem.team.logo
+    logo: scoreItem.team.logo,
+    record: scoreItem.records.find(r => r.type === 'total').summary
   };
 }
 
